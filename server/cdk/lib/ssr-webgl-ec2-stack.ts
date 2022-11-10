0199fd6fbe643f6c7288d65165c312ca15d397ca
Jayson Nabor <jayson@blockrenovation.com>
2022-11-10 04:11:08 -0500

    updated ec2 stack

--- server/cdk/lib/ssr-webgl-ec2-stack.ts
+++ server/cdk/lib/ssr-webgl-ec2-stack.ts
@@ -6,7 +6,6 @@ import { Construct } from 'constructs';
 import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { IpAddresses } from 'aws-cdk-lib/aws-ec2';
 
 export class SsrWebGlEc2Stack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,7 +25,7 @@ export class SsrWebGlEc2Stack extends cdk.Stack {
         });
 
         const vpc = new ec2.Vpc(this, 'SsrWebGlVpc', {
-            ipAddresses: IpAddresses.cidr(cidrBlock),
+            cidr: cidrBlock,
             subnetConfiguration: [
                 {
                     subnetType: ec2.SubnetType.PUBLIC,
@@ -47,6 +46,7 @@ export class SsrWebGlEc2Stack extends cdk.Stack {
             ec2.Port.tcp(80),
             'allow ingress http traffic',
         );
+
         sg.addIngressRule(
             ec2.Peer.anyIpv4(),
             ec2.Port.tcp(22),
@@ -70,8 +70,13 @@ export class SsrWebGlEc2Stack extends cdk.Stack {
             ],
         });
 
+        /* 
+            This is a private ami with ubuntu 18.04 base image preinstalled with the ff:
+            nvidia drivers, nvm (+ node.js 16.x), aws cli (+configuration), chrome
+        */
         const ami = ec2.MachineImage.lookup({
-            name: 'nv-ubuntu-18-lts-cloudGaming-510.68.02-v202204250312-offer-31487f48-b87f-4b97-ac6b-df34a9569256',
+            name: 'ubuntu-18.04-aws-nvidia-chrome',
+            // owners: ['XXXXXXXXXXXX']
         });
 
         const keyPair = new ec2.CfnKeyPair(this, 'SsrWebGlKeyPair', {
@@ -92,17 +97,15 @@ export class SsrWebGlEc2Stack extends cdk.Stack {
         });
 
         instance.userData.addCommands(
-            'exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1',
-            'echo USER DATA START',
             ...baseCommands,
+            'cd home/ubuntu/',
             'rm -rf ssr-webgl',
             'mkdir ssr-webgl',
-            // `aws s3 cp s3://${bucket.bucketName}/ssr-webgl ssr-webgl --recursive`,
-            // 'cd ssr-webgl',
-            // 'npm install',
-            // 'npm run build',
-            // 'npm run start',
-            'echo USER DATA END',
+            `aws s3 cp s3://${bucket.bucketName}/ssr-webgl /home/ubuntu/ssr-webgl --recursive`,
+            'cd ssr-webgl',
+            'npm install',
+            'npm run build',
+            'npm run start',
         );
 
         new cdk.CfnOutput(this, 'bucketName', { value: bucket.bucketName });
@@ -116,12 +119,6 @@ export class SsrWebGlEc2Stack extends cdk.Stack {
 }
 
 const baseCommands = [
-    'yum -y update',
-    'yum install -y awslogs',
-    'systemctl enable awslogsd.service',
-    'systemctl start awslogsd',
-    'aws logs create-log-group --log-group-name block-crs --region us-east-1',
-    "sed -i 's/us-east-1/g' /etc/awslogs/awscli.conf",
     'iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080',
     'iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT',
     'iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT',
--- server/cdk/update-files.sh
+++ server/cdk/update-files.sh
@@ -6,6 +6,5 @@ rm -r ssr-webgl/*
 
 cp -a ../ssr-webgl/src/ ssr-webgl/src/
 cp ../ssr-webgl/package.json ssr-webgl/package.json
-cp ../ssr-webgl/package-lock.json ssr-webgl/package-lock.json
 cp ../ssr-webgl/tsconfig.json ssr-webgl/tsconfig.json
 cp ../ssr-webgl/.puppeteerrc.cjs ssr-webgl/.puppeteerrc.cjs
